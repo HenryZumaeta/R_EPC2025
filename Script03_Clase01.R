ba8922b17369f990957da8306edca63f98bf8030
Henry Zumaeta <henryzumaeta@gmail.com>
2025-06-19 06:44:26 +0000

    Algunos paquetes populares para tener en cuenta

---
+++ Script03_Clase01.R
@@ -0,0 +1,11 @@
+# Paquetes base
+ # stats: Funciones estadísticas básicas
+ # graphis: Herramientas para gráficos base (estandar)
+ # utils: Utilidades para manipulación de datos e instalación de paquetes
+
+# Paquetes populares
+ # ggplot2
+ # dplyr
+ # caret
+ # shiny
+ # forecast
