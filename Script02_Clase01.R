36dae3bff6c0f64050a5d5165b9be4d0065cedd8
Henry Zumaeta <henryzumaeta@gmail.com>
2025-06-19 06:39:46 +0000

    Herramientas usadas

--- Script02_Clase01.R
+++ Script02_Clase01.R
@@ -1,3 +1,13 @@
 #### Herramientas en línea para programar con R ####
 # Posit Cloud
 # Notebook de Google Colab
+# Notebook de Kaggle
+# MyBinder
+# deepnote
+# paperspace
+# SageMaker (AWS)
+# Watson Studio (IBM)
+# CoCalc
+# SageMath
+# mliar
+# labex
